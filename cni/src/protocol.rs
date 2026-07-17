1e74dcacae0d0ba9610904d6f49b1d7f77850540
leo.torres <leo.torres@etu.umontpellier.fr>
2026-07-17 13:21:36 +0200

    feat: init cni

---
+++ cni/src/protocol.rs
@@ -0,0 +1,23 @@
+use proto::cni::v1::{CniRequest, CniResponse};
+
+#[derive(Debug)]
+pub struct ProtocolError;
+
+impl std::fmt::Display for ProtocolError {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        formatter.write_str("CNI protocol error")
+    }
+}
+
+impl std::error::Error for ProtocolError {}
+
+pub fn read_request(_input: impl std::io::Read) -> Result<CniRequest, ProtocolError> {
+    todo!("Decode a request from the CNI process input")
+}
+
+pub fn write_response(
+    _output: impl std::io::Write,
+    _response: &CniResponse,
+) -> Result<(), ProtocolError> {
+    todo!("Encode a response to the CNI process output")
+}
