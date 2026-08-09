87d8bddf2e66dcd6e779c0a0ba4def0ba2d98338
jessy.latmi <jessy.latmi@etu.umontpellier.fr>
2026-08-09 14:23:14 +0200

    feat: uniformize proto import path

--- proto/build.rs
+++ proto/build.rs
@@ -6,8 +6,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             "barectl/v1/kubectl.proto",
             "cni/v1/cni.proto",
             "scheduler/v1/scheduler.proto",
+            "shared/v1/container.proto",
+            "shared/v1/node.proto",
+            "shared/v1/pod.proto",
+            "shared/v1/resources.proto",
         ],
-        &[".", "..", "shared/v1"],
+        &["proto"],
     )?;
     Ok(())
 }
