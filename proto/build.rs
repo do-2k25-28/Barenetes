c2deb06a2dd310f78ae219642781241a81c2433f
ghadi saab <ghadisaab21@gmail.com>
2026-09-01 20:42:07 +0200

    feat(scheduler): schedule pods via the API server's watch streams (#82)

--- proto/build.rs
+++ proto/build.rs
@@ -16,7 +16,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             "agent/v1/kubelet.proto",
             "api/v1/api.proto",
             "cni/v1/cni.proto",
-            "scheduler/v1/scheduler.proto",
             "shared/v1/container.proto",
             "shared/v1/node.proto",
             "shared/v1/pod.proto",
