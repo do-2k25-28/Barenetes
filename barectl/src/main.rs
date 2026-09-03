7563c4fd7ca335aa9450766084a3acfeaa3b0d0f
Hugo Goetz <114746511+GoetzHugo@users.noreply.github.com>
2026-09-03 13:22:16 +0200

    feat: (barectl): kubectl-style CLI (verb + resource) (#105)
    Co-authored-by: Hugo Goetz <hugo.goetz@etu.umontpellier.fr>

--- barectl/src/main.rs
+++ barectl/src/main.rs
@@ -5,17 +5,23 @@ mod commands;
 mod error;
 mod manifest;
 
-use cli::{Cli, Commands};
+use cli::{Cli, Commands, CreateResource, DeleteResource, GetResource};
 
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
 
     let result = match cli.command {
-        Commands::CreatePod(args) => commands::create_pod(&cli.server, args).await,
-        Commands::GetPod(args) => commands::get_pod(&cli.server, args).await,
-        Commands::DeletePod(args) => commands::delete_pod(&cli.server, args).await,
-        Commands::GetNode(args) => commands::get_node(&cli.server, args).await,
+        Commands::Create(args) => match args.resource {
+            CreateResource::Pod(args) => commands::create_pod(&cli.server, args).await,
+        },
+        Commands::Get(args) => match args.resource {
+            GetResource::Pod(args) => commands::get_pod(&cli.server, args).await,
+            GetResource::Node(args) => commands::get_node(&cli.server, args).await,
+        },
+        Commands::Delete(args) => match args.resource {
+            DeleteResource::Pod(args) => commands::delete_pod(&cli.server, args).await,
+        },
     };
 
     if let Err(err) = result {
